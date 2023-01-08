7647f5dfdd7a23ee8392d59ac8267897ee78c13f
CocaMoe76 <jcstelly@outlook.com>
2023-01-08 14:31:27 -0500

    Updated archive

--- application-lynxrms/src/main/java/lynx/backend/developer/DataSeeder.java
+++ application-lynxrms/src/main/java/lynx/backend/developer/DataSeeder.java
@@ -1,27 +1,27 @@
-package lynx.backend.developer;
-
-import io.lynxrms.backend.user.User;
-import io.lynxrms.backend.user.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class DataSeeder implements CommandLineRunner {
-
-    @Autowired
-    private UserService userService;
-
-    @Override
-    public void run(String... strings) throws Exception {
-        User user = new User("jack","password","smith@lynxrm.io");
-        userService.saveUser(new User("SYSTEM","password","developer@lynxrm.io"));
-        userService.saveUser(user);
-        userService.saveUser(new User("dave","password","dave@lynxrm.io"));
-        userService.saveUser(new User("smith","password","smith@lynxrm.io"));
-        userService.saveUser(new User("bob","password","bob@lynxrm.io"));
-        userService.saveUser(new User("sam","password","sam@lynxrm.io"));
-        System.out.println("TEST USER DATA SEEDED");
-    }
-}
+//package lynx.backend.developer;
+//
+//import io.lynxrms.backend.user.User;
+//import io.lynxrms.backend.user.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class DataSeeder implements CommandLineRunner {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Override
+//    public void run(String... strings) throws Exception {
+//        User user = new User("jack","password","smith@lynxrm.io");
+//        userService.saveUser(new User("SYSTEM","password","developer@lynxrm.io"));
+//        userService.saveUser(user);
+//        userService.saveUser(new User("dave","password","dave@lynxrm.io"));
+//        userService.saveUser(new User("smith","password","smith@lynxrm.io"));
+//        userService.saveUser(new User("bob","password","bob@lynxrm.io"));
+//        userService.saveUser(new User("sam","password","sam@lynxrm.io"));
+//        System.out.println("TEST USER DATA SEEDED");
+//    }
+//}
 
